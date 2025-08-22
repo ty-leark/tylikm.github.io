6f01edd807bc4e9b7e88cd20ebf0e108347167eb
ty-leark <tylikmawyer16@gmail.com>
2025-08-21 22:59:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,27 +16,39 @@ const highlightMenu = () => {
     const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
     const servicesMenu = document.querySelector('#services-page');
-    const educationMenu = document.querySelector('#education-page')
+    const educationMenu = document.querySelector('#education-page');
     let scrollPos = window.scrollY;
-    // console.log(scrollPos);
+    console.log(scrollPos);
 
     // Adds Highlight to menu items
     if(window.innerWidth > 960 && scrollPos < 600) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        educationMenu.classList.remove('highlight');
         return;
     }   else if (window.innerWidth > 960 && scrollPos < 1400) {
-        aboutMenu.classList.add('highlight');
         homeMenu.classList.remove('highlight');
+        aboutMenu.classList.add('highlight');
         servicesMenu.classList.remove('highlight');
+        educationMenu.classList.remove('highlight');
         return;
     }   else if (window.innerWidth > 960 && scrollPos < 2345) {
-        servicesMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
         aboutMenu.classList.remove('highlight');
+        servicesMenu.classList.add('highlight');
+        educationMenu.classList.remove('highlight');
         return;
-    }
-    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    }   else if (window.innerWidth > 960 && scrollPos < 3400) {
+        homeMenu.classList.remove('highlight');
+        aboutMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        educationMenu.classList.add('highlight');
+        return;
+    }   
+        if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
         elem.classList.remove('highlight');
+        return;
     }
 }
 
@@ -47,7 +59,7 @@ window.addEventListener('click', highlightMenu);
 const hideMobileMenu = () => {
     const menuBars = document.querySelector('.is-active');
     if(window.innerWidth <= 768 && menuBars) {
-        menu.classList.toggle('is-acitve');
+        menu.classList.toggle('is-active');
         menuLinks.classList.remove('active');
     }
 }
